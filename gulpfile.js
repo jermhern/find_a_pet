f9c2ff640c2531e6d96b08ea251644fa800c8743
jeremy hernandez <jermhern@gmail.com>
2018-07-02 21:09:33 -0700

    added bootstrap cdn

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+/*eslint-disable*/
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('default', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+        }
+    });
+
+    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./css/*.css").on('change', browserSync.reload);
+});
